a243db466fc9bf13690eaaa6280eae3e4268257f
Danny Zhang <dz149131@gmail.com>
2022-11-01 17:49:11 -0500

    update file

--- HTML_CSS_JavaScript - Personal Site/JavaScript/javascript.js
+++ HTML_CSS_JavaScript - Personal Site/JavaScript/javascript.js
@@ -784,7 +784,7 @@ const winningScoreSelect = document.querySelector('#winningScoreCount')
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function () {
