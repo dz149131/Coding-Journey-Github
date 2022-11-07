161b4012390f7c5ab2c464c451d7d1acd7b00485
Danny Zhang <dz149131@gmail.com>
2022-11-07 15:17:33 -0600

    Await / Handling Async Function errors Notes

--- HTML_CSS_JavaScript - Personal Site/JavaScript/javascript.js
+++ HTML_CSS_JavaScript - Personal Site/JavaScript/javascript.js
@@ -1039,3 +1039,60 @@ winningScoreSelect.addEventListener('change', function(){
 //         })
 //     })
 // });
+
+// const login = async (username, password) => {
+//     if (!username || !password) throw 'Missing Credentials'
+//     if (password === 'password') return 'Successful login'
+//     throw 'Invalid Password'
+// }
+// login('username', 'password')
+// .then(msg => {
+//     console.log("Logged in")
+//     console.log(msg)
+// })
+// .catch(err => {
+//     console.log("Error")
+//     console.log(err)
+// })
+// // return = promise resolved
+// // throw = promise rejected
+                                    //AWAIT
+// async function rainbow () {
+//     await bgColorChange('red', 1000)
+//     await bgColorChange('orange', 1000)
+//     await bgColorChange('yellow', 1000)
+//     await bgColorChange('green', 1000)
+//     await bgColorChange('blue', 1000)
+//     await bgColorChange('indigo', 1000)
+//     await bgColorChange('violet', 1000)
+// }   
+
+// async function printRainbow() {
+//     await rainbow()
+//     console.log("End of rainbow")
+// }
+
+                                    //HANDLING ERRORS IN ASYNC FUNCTIONS
+// const fakeRequestPromise = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const delay = Math.floor(Math.random() * (4500)) + 500;
+//         setTimeout(() => {
+//             if (delay > 4000) {
+//                 reject('Connection Timeout :(')
+//             } else {
+//                 resolve(`Here is your fake data from ${url}`)
+//             }
+//         }, delay)
+//     })
+// }
+// async function makeTwoRequests() {
+//     try {
+//         let data1 = await fakeRequest('/page1');
+//         console.log(data1);
+//         let data2 = await fakeRequest('/page2');
+//         console.log(data2);
+//     } catch (e) {
+//         console.log("Caught an error")
+//         console.log("error is:", e)
+//     }
+// }
