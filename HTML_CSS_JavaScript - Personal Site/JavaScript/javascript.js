4f3d1072c64a2cff9273951dac415ffc1fba94fa
Danny Zhang <dz149131@gmail.com>
2022-11-07 15:22:32 -0600

    Updated img for 'error_async_functions'

--- HTML_CSS_JavaScript - Personal Site/JavaScript/javascript.js
+++ HTML_CSS_JavaScript - Personal Site/JavaScript/javascript.js
@@ -1073,26 +1073,26 @@ winningScoreSelect.addEventListener('change', function(){
 // }
 
                                     //HANDLING ERRORS IN ASYNC FUNCTIONS
-// const fakeRequestPromise = (url) => {
-//     return new Promise((resolve, reject) => {
-//         const delay = Math.floor(Math.random() * (4500)) + 500;
-//         setTimeout(() => {
-//             if (delay > 4000) {
-//                 reject('Connection Timeout :(')
-//             } else {
-//                 resolve(`Here is your fake data from ${url}`)
-//             }
-//         }, delay)
-//     })
-// }
-// async function makeTwoRequests() {
-//     try {
-//         let data1 = await fakeRequest('/page1');
-//         console.log(data1);
-//         let data2 = await fakeRequest('/page2');
-//         console.log(data2);
-//     } catch (e) {
-//         console.log("Caught an error")
-//         console.log("error is:", e)
-//     }
-// }
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+async function makeTwoRequests() {
+    try {
+        let data1 = await fakeRequest('/page1');
+        console.log(data1);
+        let data2 = await fakeRequest('/page2');
+        console.log(data2);
+    } catch (e) {
+        console.log("Caught an error")
+        console.log("error is:", e)
+    }
+}
