2cd21f361d13fc2e2d7a0414d2297ee1f0ca4eba
Danny Zhang <dz149131@gmail.com>
2022-11-07 19:55:01 -0600

    Navbar fix for 'AJax & APIs'

--- HTML_CSS_JavaScript - Personal Site/JavaScript/javascript.js
+++ HTML_CSS_JavaScript - Personal Site/JavaScript/javascript.js
@@ -1096,6 +1096,7 @@ winningScoreSelect.addEventListener('change', function(){
 //         console.log("error is:", e)
 //     }
 // }
+
 // //THIS IS A STRING OF JSON (NOT AN OBJECT)
 // const data = `{"ticker":{"base":"BTC","target":"USD","price":"11288.49813464","volume":"91769.69699773","change":"-46.29462447"},"timestamp":1596510482,"success":true,"error":""}`
 
