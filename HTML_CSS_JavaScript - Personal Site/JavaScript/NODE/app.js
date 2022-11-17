defdd2ad6c42c48804a54d282397acc50e655e0c
Danny Zhang <dz149131@gmail.com>
2022-11-16 19:05:02 -0600

    Added 'module.exports' Notes

---
+++ HTML_CSS_JavaScript - Personal Site/JavaScript/NODE/app.js
@@ -0,0 +1,7 @@
+// const math = require('./math');
+const {PI, square} = require('./math'); //destructured 
+// console.log(math.PI)
+console.log(PI)
+
+// console.log(math.square(9))
+console.log(square(9))
---
+++ HTML_CSS_JavaScript - Personal Site/JavaScript/NODE/math.js
@@ -0,0 +1,18 @@
+const add = (x,y) => x + y;
+// module.exports.add = (x,y) => x + y;
+
+const PI = 3.14159;
+// module.exports.PI = 3.14159;
+
+const square = x => x * x;
+// module.exports.square = x => x * x;
+
+const math = {
+    add: add,
+    PI: PI,
+    square: square
+}
+module.exports = math;
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
