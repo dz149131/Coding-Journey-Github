631c15dfd7d75bac7f589ac5569387a0d91b3a30
Danny Zhang <dz149131@gmail.com>
2022-11-22 19:41:06 -0600

    Added 'colorsNPM' to 'JokesNPM', renamed img.file for better clarification

--- HTML_CSS_JavaScript - Personal Site/JavaScript/NODE/JokesNPM/index.js
+++ HTML_CSS_JavaScript - Personal Site/JavaScript/NODE/JokesNPM/index.js
@@ -1,5 +1,6 @@
 const jokes = require("give-me-a-joke");
 // console.dir(jokes)
+const colors = require("colors")
 jokes.getRandomDadJoke (function (joke) {
-    console.log(joke);
+    console.log(joke.rainbow);
 });
