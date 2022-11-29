88e97744397c7b008209a4de8e548e5406ac4ddf
Danny Zhang <dz149131@gmail.com>
2022-11-28 22:20:04 -0600

    Added 'language guesser project' notes

---
+++ HTML_CSS_JavaScript - Personal Site/JavaScript/NODE/NPM/Language Guesser Project/index.js
@@ -0,0 +1,7 @@
+const langs = require("langs");
+const franc = import {franc, francAll} from 'franc'
+console.log(franc('Alle menslike wesens word vry'));
+
+// const langCode = console.log(franc('Alle menslike wesens word vry'));
+// const language = langs.where("3", langCode);
+// console.log(language.name)
