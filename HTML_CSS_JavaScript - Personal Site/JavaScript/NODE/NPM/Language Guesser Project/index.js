1ee64425d37577ee942190ddbee5fe9a6832f05c
Danny Zhang <dz149131@gmail.com>
2022-11-29 22:36:56 -0600

    Added 'language guesser project' notes, fixed 'index.js' RefrenceError

--- HTML_CSS_JavaScript - Personal Site/JavaScript/NODE/NPM/Language Guesser Project/index.js
+++ HTML_CSS_JavaScript - Personal Site/JavaScript/NODE/NPM/Language Guesser Project/index.js
@@ -1,7 +1,12 @@
-const langs = require("langs");
-const franc = import {franc, francAll} from 'franc'
-console.log(franc('Alle menslike wesens word vry'));
-
-// const langCode = console.log(franc('Alle menslike wesens word vry'));
-// const language = langs.where("3", langCode);
-// console.log(language.name)
+import {franc, francAll} from 'franc';
+import langs from 'langs';
+import colors from 'colors';
+const colors = require("colors")
+const input = process.argv[2];
+const langCode = franc(input);
+if (langCode === 'und'){
+    console.log("Error! Enter larger sample text.".red)
+} else {
+    const language = langs.where("3", langCode);
+    console.log(language.name.green)
+}
