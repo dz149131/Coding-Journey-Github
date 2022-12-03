77f41015bce20202f547b22c9b9634a088c93f94
Danny Zhang <dz149131@gmail.com>
2022-12-03 16:52:59 -0600

    Added 'Express Routing Basics' notes

--- HTML_CSS_JavaScript - Personal Site/JavaScript/NODE/express/FirstApp/index.js
+++ HTML_CSS_JavaScript - Personal Site/JavaScript/NODE/express/FirstApp/index.js
@@ -2,13 +2,32 @@ const express = require("express");
 const app = express();
 // console.dir(app)
 
-app.use((req, res) => {                                         //Express creates javascript object automatically 
-    console.log("We got a new request")                         //for us by parsing the incoming http request 
-    // console.dir(req)                                         //information and passes it into the first argument 
-    res.send("Hello, we got your request. This is a response.") //to this call back
-    // res.send("<h1>Heading created from res.send</h1>")
-    // res.send({color: 'red'})                                 //content type = application/json
+// app.use((req, res) => {                                         //Express creates javascript object automatically 
+//     console.log("We got a new request")                         //for us by parsing the incoming http request 
+//     // console.dir(req)                                         //information and passes it into the first argument 
+//     res.send("Hello, we got your request. This is a response.") //to this call back
+//     // res.send("<h1>Heading created from res.send</h1>")
+//     // res.send({color: 'red'})                                 //content type = application/json
+// })
+
+app.get('/', (req, res) => {
+    res.send("This is the home page")
 })
+app.get('/cats', (req, res) => { 
+    console.log("CAT REQUEST")
+    res.send('MEOW!')
+})
+
+app.post('/cats', (req, res) => {
+    res.send('POST REQUEST TO /cats')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF!')
+})
+
+
+
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000")
 })
