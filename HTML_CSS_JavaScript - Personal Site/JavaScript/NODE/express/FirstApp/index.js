b0957e05a1ee1557e8442619102fe6ff61b680a1
Danny Zhang <dz149131@gmail.com>
2022-12-03 16:16:43 -0600

    Added 'req_res_object' notes

--- HTML_CSS_JavaScript - Personal Site/JavaScript/NODE/express/FirstApp/index.js
+++ HTML_CSS_JavaScript - Personal Site/JavaScript/NODE/express/FirstApp/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 // console.dir(app)
 
-app.use(() => {
-    console.log("We got a new request")
+app.use((req, res) => {                                         //Express creates javascript object automatically 
+    console.log("We got a new request")                         //for us by parsing the incoming http request 
+    // console.dir(req)                                         //information and passes it into the first argument 
+    res.send("Hello, we got your request. This is a response.") //to this call back
+    // res.send("<h1>Heading created from res.send</h1>")
+    // res.send({color: 'red'})                                 //content type = application/json
 })
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000")
