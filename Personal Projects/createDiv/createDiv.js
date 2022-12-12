0987603d9632119388179911c859c225d164a3fd
Danny Zhang <dz149131@gmail.com>
2022-12-11 18:07:16 -0600

    Added 'createDiv' project

--- Personal Projects/createDiv/createDiv.js
+++ Personal Projects/createDiv/createDiv.js
@@ -1,10 +1,21 @@
-const createDivBtn = document.getElementById('#createDivBtn');
-createDivBtn.addEventListener('click', function() {
-    const newDiv = document.createElement('div');
-    newDiv.style.width = '100px';
-    newDiv.style.height = '100px';
-    newDiv.style.backgroundColor = 'blue';
-    newDiv.style.border = '1px solid black';
-});
+// const createDivBtn = document.getElementById('#createDivBtn');
+// createDivBtn.addEventListener('click', function() {
+//     let newDiv = document.createElement('div');
+//     newDiv.style.width = '100px';
+//     newDiv.style.height = '100px';
+//     newDiv.style.backgroundColor = 'blue';
+//     newDiv.style.border = '1px solid black';
+// });
 
-document.getElementById('#container').appendChild(newDiv);
+// const container = document.getElementById('#container');
+// container.appendChild(newDiv);
+let colorPicker = document.getElementById('#colorPicker')
+let widthInput = document.getElementById('#widthInput')
+let heightInput = document.getElementById('#heightInput')
+let resetBtn = document.getElementById('#resetBtn')
+let createBtn = document.getElementById('#createBtn')
+let divContainer = document.getElementById('#divContainer')
+
+createBtn.addEventListener('click', function() {
+    divContainer.innerHTML = '';
+})
