8cea4cc47bfa9fc42267a0baa6f9f3d841ff577f
Danny Zhang <dz149131@gmail.com>
2022-12-18 22:42:16 -0600

    Edited Personal project 'createDiv'

--- Personal Projects/createDiv/createDiv.js
+++ Personal Projects/createDiv/createDiv.js
@@ -9,13 +9,32 @@
 
 // const container = document.getElementById('#container');
 // container.appendChild(newDiv);
-let colorPicker = document.getElementById('#colorPicker')
-let widthInput = document.getElementById('#widthInput')
-let heightInput = document.getElementById('#heightInput')
-let resetBtn = document.getElementById('#resetBtn')
-let createBtn = document.getElementById('#createBtn')
-let divContainer = document.getElementById('#divContainer')
+// let colorPicker = document.getElementById('#colorPicker')
+// let widthInput = document.getElementById('#widthInput')
+// let heightInput = document.getElementById('#heightInput')
+// let resetBtn = document.getElementById('#resetBtn')
+// let createBtn = document.getElementById('#createBtn')
+// let divContainer = document.getElementById('#divContainer')
 
-createBtn.addEventListener('click', function() {
-    divContainer.innerHTML = '';
+// createBtn.addEventListener('click', function() {
+//     divContainer.innerHTML = '';
+// })
+
+const createDiv = document.querySelector('#createDiv');
+createDiv.addEventListener('click', function() {
+    //Gets user inputed values from form
+    let width = document.getElementById('#width').value;
+    let height = document.getElementById('#height').value;
+    let color = document.getElementById('#color').value;
+    
+    //creates new div 
+    let div = document.createElement('div');
+
+    //set the div's style properties
+    div.style.width = width;
+    div.style.height = height;
+    div.style.backgroundColor = color;
+
+    //append new div to page
+    document.body.appendChild(div);
 })
