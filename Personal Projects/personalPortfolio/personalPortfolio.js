507837867eb0f23fd287bdec154b8123b729084a
Danny Zhang <dz149131@gmail.com>
2023-01-11 18:30:17 -0600

    moved html and css notes to 'htmlCSS' directory

---
+++ Personal Projects/personalPortfolio/personalPortfolio.js
@@ -0,0 +1,12 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+const navbar = document.getElementsByClassName('navbar')
+
+navbar.addEventListener('scroll', () => {
+    
+})
