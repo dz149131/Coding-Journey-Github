8ef82f870003b9e509ec832ac9dc8cf70f47bb97
Danny Zhang <dz149131@gmail.com>
2023-01-30 17:50:01 -0600

    ejs notes

--- Javascript Notes/NODE/templateEjs/index.js
+++ Javascript Notes/NODE/templateEjs/index.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', {rand: num})
+})
+
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000")
 })
