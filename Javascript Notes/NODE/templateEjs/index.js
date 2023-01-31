1ea56137e3d3988efb0fc69870659cd87bc606e5
Danny Zhang <dz149131@gmail.com>
2023-01-30 20:36:37 -0600

    ejs static assets notes

--- Javascript Notes/NODE/templateEjs/index.js
+++ Javascript Notes/NODE/templateEjs/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const redditData = require('./data.json');
 
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
