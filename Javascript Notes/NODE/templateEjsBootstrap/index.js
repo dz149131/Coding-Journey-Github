29e75cf10dbe3b4dd87c517dd03c4468f9ae4e43
Danny Zhang <dz149131@gmail.com>
2023-01-30 20:42:21 -0600

    created branch folder for bootstrap

---
+++ Javascript Notes/NODE/templateEjsBootstrap/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const redditData = require('./data.json');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Milo', 'Turbo', 'Moose', 'Red'
+    ]
+    res.render('cats', {cats})
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const {subreddit} = req.params;
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', {...data}); //... = spread open
+    } else {
+        res.render('notfound', {subreddit}) //another webpage*
+    }
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', {rand: num})
+})
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000")
+})
