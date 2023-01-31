54409c9843a0bb514fc839f281b8da815278d801
Danny Zhang <dz149131@gmail.com>
2023-01-30 20:18:03 -0600

    ejs notes subreddit demo

--- Javascript Notes/NODE/templateEjs/index.js
+++ Javascript Notes/NODE/templateEjs/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
@@ -9,9 +11,21 @@ app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Milo', 'Turbo', 'Moose', 'Red'
+    ]
+    res.render('cats', {cats})
+})
+
 app.get('/r/:subreddit', (req, res) => {
     const {subreddit} = req.params;
-    res.render('subreddit', {subreddit});
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', {...data}); //... = spread open
+    } else {
+        res.render('notfound', {subreddit}) //another webpage*
+    }
 })
 
 app.get('/rand', (req, res) => {
