085c229d278c94f1cc25d143c9e5e29908717fb4
Danny Zhang <dz149131@gmail.com>
2023-01-30 18:28:14 -0600

    ejs conditionals notes

--- Javascript Notes/NODE/templateEjs/index.js
+++ Javascript Notes/NODE/templateEjs/index.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.render('home.ejs')
 })
 
+app.get('/r/:subreddit', (req, res) => {
+    const {subreddit} = req.params;
+    res.render('subreddit', {subreddit});
+})
+
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
     res.render('random', {rand: num})
