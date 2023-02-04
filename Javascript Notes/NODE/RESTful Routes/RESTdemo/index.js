16eb3d1b8c489fc5907c0ff0136d8e81d5936eb6
Danny Zhang <dz149131@gmail.com>
2023-02-04 03:45:53 -0600

    REST notes

--- Javascript Notes/NODE/RESTful Routes/RESTdemo/index.js
+++ Javascript Notes/NODE/RESTful Routes/RESTdemo/index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lorem lorem lorem lorem lorem'
+    },
+    {
+        username: 'Tuttifrutti',
+        comment: 'We must fall back into the trenches'
+    },
+    {
+        username: 'undergroundslayer55',
+        comment: 'I like the new central centering device released'
+    },
+    {
+        username: 'Kachow',
+        comment: 'Cars 3 is the best movie'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', {comments})
+})
 
 app.get('/tacos', (req, res) => {
     res.send("GET /taco response")
