a291af5fe7b72562f1ea4dff1f5bd7734f90b68e
Danny Zhang <dz149131@gmail.com>
2023-02-03 19:07:28 -0600

    RESTful routes notes

--- Javascript Notes/NODE/RESTful Routes/RESTdemo/index.js
+++ Javascript Notes/NODE/RESTful Routes/RESTdemo/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.get('/tacos', (req, res) => {
     res.send("GET /taco response")
 })
 
 app.post('/tacos', (req, res) => {
-    res.send("POST / taco response")
+    // console.log(req.body)
+    const { meats, qty } = req.body;
+    // res.send("POST / taco response")
+    res.send(`Ok, here  are your ${qty} ${meats} tacos`)
 })
 
 app.listen(3000, () => {
