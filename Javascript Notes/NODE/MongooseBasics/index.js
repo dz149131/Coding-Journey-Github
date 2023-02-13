3a1cfdd3c88e3d0d22fd2450097a29f954d3937d
Danny Zhang <dz149131@gmail.com>
2023-02-13 17:36:04 -0600

    mongoose notes

--- Javascript Notes/NODE/MongooseBasics/index.js
+++ Javascript Notes/NODE/MongooseBasics/index.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
-.then (() => {
-    console.log("Connection Open")
-})
-.catch (err => {
-    console.log("Error!")
-    console.log(err)
-})
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!!")
+        console.log(err)
+    })
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+const it = new Movie ({ title: 'It', year: 2011, score: 9.2, rating: 'R' });
