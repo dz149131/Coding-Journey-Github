dfec302fd817e7901afc06912e6b1651a1a1df2a
Danny Zhang <dz149131@gmail.com>
2023-02-23 13:18:13 -0600

    mongoose+express notes

---
+++ Javascript Notes/NODE/Express_Mongoose/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://127.0.0.1:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION SUCCESSFUL")
+    })
+    .catch(err => {
+        console.log("MONGO ERROR")
+        console.log(err)
+    })
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/test', (req, res) => {
+    res.send('test!')
+})
+
+app.listen(3000, () => {
+    console.log("Listening On Port 3000")
+})
