63d07619e2f6f0a87092b63a84bc29fd7830bd6b
Danny Zhang <dz149131@gmail.com>
2023-02-23 16:01:56 -0600

    product index notes

--- Javascript Notes/NODE/Express_Mongoose/index.js
+++ Javascript Notes/NODE/Express_Mongoose/index.js
@@ -18,8 +18,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/groceryStore', { useNewUrlParser: tr
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/test', (req, res) => {
-    res.send('test!')
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', {products})
 })
 
 app.listen(3000, () => {
