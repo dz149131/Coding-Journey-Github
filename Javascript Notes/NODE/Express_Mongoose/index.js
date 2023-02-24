5c073906da58ab8391956160d7d18dc7ff1f4a2c
Danny Zhang <dz149131@gmail.com>
2023-02-24 15:59:17 -0600

    Filtering Products notes

--- Javascript Notes/NODE/Express_Mongoose/index.js
+++ Javascript Notes/NODE/Express_Mongoose/index.js
@@ -25,8 +25,14 @@ app.use(methodOverride('_method'))
 const categories = ['fruit', 'vegetable', 'dairy']
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', {products})
+    const {category} = req.query;
+    if(category){
+        const products = await Product.find({category})
+        res.render('products/index', {products, category})
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', {products, category: 'All'})
+    }
 })
 
 app.get('/products/new', (req, res) => {
