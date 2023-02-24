767ca28570038e96aed7955d10c134ca742ccaab
Danny Zhang <dz149131@gmail.com>
2023-02-24 17:12:54 -0600

    YelpCamp project

---
+++ Javascript Notes/YelpCamp/app.js
@@ -0,0 +1,16 @@
+//npm init -y
+//npm i Express Mongoose EJS
+const express = require('express');
+const app = express()
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000, () => {
+    console.log('Serving on Port 3000')
+})
