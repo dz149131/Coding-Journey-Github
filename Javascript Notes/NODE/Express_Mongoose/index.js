a721545b887493fee317e02ee8de8e8ba64fbb2d
Danny Zhang <dz149131@gmail.com>
2023-02-23 18:00:34 -0600

    Update Product notes

--- Javascript Notes/NODE/Express_Mongoose/index.js
+++ Javascript Notes/NODE/Express_Mongoose/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 const Product = require('./models/product');
 
@@ -17,7 +18,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/groceryStore', { useNewUrlParser: tr
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
@@ -40,6 +43,19 @@ app.get('/products/:id', async (req, res) => {
     console.log(product);
     res.render('products/show', {product})
 })
+
+app.get('/products/:id/edit', async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', {product})
+})
+
+app.put('/products/:id', async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+    res.redirect(`/products/${product._id}`)
+})
+
 
 app.listen(3000, () => {
     console.log("Listening On Port 3000")
