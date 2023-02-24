145e41331c1206d583c4a3e7410f2e18ca2d7146
Danny Zhang <dz149131@gmail.com>
2023-02-24 14:46:58 -0600

    category selector notes

--- Javascript Notes/NODE/Express_Mongoose/index.js
+++ Javascript Notes/NODE/Express_Mongoose/index.js
@@ -22,13 +22,15 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 
+const categories = ['fruit', 'vegetable', 'dairy']
+
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     res.render('products/index', {products})
 })
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', {categories})
 })
 
 app.post('/products', async (req, res) => {
@@ -47,7 +49,7 @@ app.get('/products/:id', async (req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
     const {id} = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', {product})
+    res.render('products/edit', {product, categories})
 })
 
 app.put('/products/:id', async (req, res) => {
