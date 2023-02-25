2987fd4b2cddfa1ee99f526faa3fa61256f132e6
Danny Zhang <dz149131@gmail.com>
2023-02-24 18:25:13 -0600

    model basics notes

--- Javascript Notes/YelpCamp/app.js
+++ Javascript Notes/YelpCamp/app.js
@@ -1,14 +1,34 @@
 //npm init -y
 //npm i Express Mongoose EJS
 const express = require('express');
-const app = express()
 const path = require('path');
+const mongoose = require('mongoose');
 
+const Campground = require('./models/campground');
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION SUCCESSFUL")
+    })
+    .catch(err => {
+        console.log("MONGO ERROR")
+        console.log(err)
+    })
+    
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('home')
+});
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title: 'Backyard', description: 'Budget camping'});
+    await camp.save();
+    res.send(camp)
 })
 
 app.listen(3000, () => {
