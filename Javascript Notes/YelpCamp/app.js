9dbe08243e2a4ec20622500f49b0db538fd24694
Danny Zhang <dz149131@gmail.com>
2023-02-27 15:10:59 -0600

    campground index notes

--- Javascript Notes/YelpCamp/app.js
+++ Javascript Notes/YelpCamp/app.js
@@ -25,11 +25,10 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: 'Backyard', description: 'Budget camping'});
-    await camp.save();
-    res.send(camp)
-})
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
+});
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000')
