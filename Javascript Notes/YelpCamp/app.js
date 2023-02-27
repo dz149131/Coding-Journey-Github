5aec6ce706ff01b679ba1df93bbfd0bce9c0af1c
Danny Zhang <dz149131@gmail.com>
2023-02-27 16:11:22 -0600

    campground new&create notes

--- Javascript Notes/YelpCamp/app.js
+++ Javascript Notes/YelpCamp/app.js
@@ -21,6 +21,10 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({extended: true}));
+
+
+
 app.get('/', (req, res) => {
     res.render('home')
 });
@@ -30,10 +34,21 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', {campgrounds})
 });
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async(req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', {campground});
 })
+
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000')
