3c08c5b7d38e8a693b810f4c932c11d2753262fe
Danny Zhang <dz149131@gmail.com>
2023-02-27 16:46:16 -0600

    campground delete notes

--- Javascript Notes/YelpCamp/app.js
+++ Javascript Notes/YelpCamp/app.js
@@ -61,6 +61,12 @@ app.put('/campgrounds/:id', async(req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+app.delete('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
 app.listen(3000, () => {
     console.log('Serving on Port 3000')
 })
