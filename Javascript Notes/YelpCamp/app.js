caf2220ecf1d6513148a0e1bfbf8987bcbd7886b
Danny Zhang <dz149131@gmail.com>
2023-02-27 16:39:35 -0600

    campground edit&update notes

--- Javascript Notes/YelpCamp/app.js
+++ Javascript Notes/YelpCamp/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 const Campground = require('./models/campground');
 
@@ -22,7 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
-
+app.use(methodOverride('_method'));
 
 
 app.get('/', (req, res) => {
@@ -49,6 +50,16 @@ app.get('/campgrounds/:id', async(req, res) => {
     res.render('campgrounds/show', {campground});
 })
 
+app.get('/campgrounds/:id/edit', async(req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', {campground});
+})
+
+app.put('/campgrounds/:id', async(req, res) => {
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000')
