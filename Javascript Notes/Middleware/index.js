ccfb7f78cc1d8ab0ca05283893a047eb8f730299
Danny Zhang <dz149131@gmail.com>
2023-02-27 21:27:35 -0600

    defining our own middleware

--- Javascript Notes/Middleware/index.js
+++ Javascript Notes/Middleware/index.js
@@ -3,7 +3,20 @@ const app = express();
 const morgan = require('morgan');
 
 
-app.use(morgan('tiny'))
+app.use(morgan('common'))
+app.use((req, res, next) => {
+    console.log("This is my first middleware")
+    return next();
+    console.log("First middleware after calling next()")
+})
+app.use((req, res, next) => {
+    console.log("This is my second middleware")
+    return next();
+})
+app.use((req, res, next) => {
+    console.log("This is my third middleware")
+    return next();
+})
 
 app.get('/', (req, res) => {
     res.send('Home Page')
