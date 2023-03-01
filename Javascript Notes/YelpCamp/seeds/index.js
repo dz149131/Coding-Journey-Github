7e654ce4a60964d9c4ab5b310b05152eb94d7ca9
Danny Zhang <dz149131@gmail.com>
2023-03-01 14:10:56 -0600

    yelp styling notes

--- Javascript Notes/YelpCamp/seeds/index.js
+++ Javascript Notes/YelpCamp/seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random () *20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/random',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolores suscipit accusamus. Sapiente repellendus aperiam nemo debitis minima ut nesciunt amet deleniti esse dicta, repudiandae, autem eaque eligendi fugit vel?',
+            price
         })
         await camp.save();
     }
