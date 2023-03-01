224a13f135cea0c179040e468a0a7c856f340c0d
Danny Zhang <dz149131@gmail.com>
2023-02-28 19:16:42 -0600

    middleware practice notes

--- Javascript Notes/Middleware/index.js
+++ Javascript Notes/Middleware/index.js
@@ -3,26 +3,35 @@ const app = express();
 const morgan = require('morgan');
 
 
-app.use(morgan('common'))
+// app.use(morgan('common'))
+
 app.use((req, res, next) => {
-    console.log("This is my first middleware")
-    return next();
-    console.log("First middleware after calling next()")
-})
-app.use((req, res, next) => {
-    console.log("This is my second middleware")
-    return next();
-})
-app.use((req, res, next) => {
-    console.log("This is my third middleware")
-    return next();
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
 })
 
+// app.use((req, res, next) => {
+//     console.log("This is my first middleware")
+//     return next();
+//     console.log("First middleware after calling next()")
+// })
+// app.use((req, res, next) => {
+//     console.log("This is my second middleware")
+//     return next();
+// })
+// app.use((req, res, next) => {
+//     console.log("This is my third middleware")
+//     return next();
+// })
+
 app.get('/', (req, res) => {
+    console.log(`Request Date: ${req.requestTime}`)
     res.send('Home Page')
 })
 
 app.get('/dogs', (req, res) => {
+    console.log(`Request Date: ${req.requestTime}`)
     res.send('Woof Woof')
 })
 
