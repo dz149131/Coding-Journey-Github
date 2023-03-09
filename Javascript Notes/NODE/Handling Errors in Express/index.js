ff5aa983714f8d6b33c1f4188b5abb330cb107da
Danny Zhang <dz149131@gmail.com>
2023-03-09 17:05:50 -0600

    Handling Errors in Express notes

---
+++ Javascript Notes/NODE/Handling Errors in Express/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+
+app.use(morgan('tiny'));
+
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+})
+
+app.use('/dogs', (req, res ,next) => {
+    console.log("I Love Dogs")
+    next();
+})
+
+const verifyPassword = (req, res , next) => {
+    const {password} = req.query;
+    if (password === 'unlock') {
+        next();
+    }
+    res.send('You Need To Enter A Password')
+}
+
+// app.use((req, res, next) => {
+//     console.log("This is my first middleware")
+//     return next();
+//     console.log("First middleware after calling next()")
+// })
+// app.use((req, res, next) => {
+//     console.log("This is my second middleware")
+//     return next();
+// })c
+// app.use((req, res, next) => {
+//     console.log("This is my third middleware")
+//     return next();
+// })
+
+app.get('/', (req, res) => {
+    console.log(`Request Date: ${req.requestTime}`)
+    res.send('Home Page')
+})
+
+app.get('/dogs', (req, res) => {
+    console.log(`Request Date: ${req.requestTime}`)
+    res.send('Woof Woof')
+})
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('You found the secret!')
+})
+
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND!')
+})
+
+app.listen(3000, () => {
+    console.log('App is running on localhost:3000')
+})
