851ffd484cf56e4f018f725db828e00e4d1da603
Danny Zhang <dz149131@gmail.com>
2023-03-13 20:17:23 -0500

    Handling Async Errors notes

--- Javascript Notes/NODE/Handling Async Errors/index.js
+++ Javascript Notes/NODE/Handling Async Errors/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 
 const categories = ['fruit', 'vegetable', 'dairy']
-
+//-------------OLD---------------------
 app.get('/products', async (req, res, next) => {
     try {
         const {category} = req.query;
@@ -39,63 +39,52 @@ app.get('/products', async (req, res, next) => {
         next(e);
     }
 })
+//-------------NEW---------------------
+function wrapAsync(fn) {
+    return function(req, res, next){
+        fn(req, res, next).catch(e => next(e))
+    }
+}
 
 app.get('/products/new', (req, res) => {
     res.render('products/new', {categories})
 })
 
-app.post('/products', async (req, res, next) => {
-    try {
-        const newProduct = new Product(req.body);
-        await newProduct.save();
-        res.redirect(`/products/${newProduct._id}`)
-    } catch (e){
-        next(e);
-    }
-})
+app.post('/products', wrapAsync(async (req, res, next) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
+}))
 
-app.get('/products/:id', async (req, res, next) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id)
-        if(!product) {
-            throw new AppError('Product Not Found', 404);
-        }
-        res.render('products/show', {product})
-    } catch (e){
-        next(e);
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
+    const {id} = req.params;
+    const product = await Product.findById(id)
+    if(!product) {
+        throw new AppError('Product Not Found', 404);
     }
-    
-})
+    res.render('products/show', {product})
+}))
 
-app.get('/products/:id/edit', async (req, res, next) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        if(!product) {
-            throw new AppError('Product Not Found', 404);
-        }
-        res.render('products/edit', {product, categories})
-    } catch (e){
-        next(e);
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    if(!product) {
+        throw new AppError('Product Not Found', 404);
     }
-})
+    res.render('products/edit', {product, categories})
+}))
 
-app.put('/products/:id', async (req, res, next) => {
-    try{
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
-        res.redirect(`/products/${product._id}`)
-    } catch (e){
-        next(e);
-    }
-})
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+    res.redirect(`/products/${product._id}`)
+}))
 
-app.delete('/products/:id', async (req, res) => {
+app.delete('/products/:id', wrapAsync(async (req, res, next) => {
     const {id} = req.params;
     const deletedProduct = await Product.findByIdAndDelete(id);
     res.redirect('/products');
-})
+}))
 
 app.use((err, req, res, next) => {
     const {status = 500, message = 'Something went wrong'} = err;
