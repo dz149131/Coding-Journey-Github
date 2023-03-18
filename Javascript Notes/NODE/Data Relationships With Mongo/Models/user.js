5a28c70a16f7ea523009eba0a4f066f18499723b
Danny Zhang <dz149131@gmail.com>
2023-03-17 20:31:26 -0500

    One to Many notes

---
+++ Javascript Notes/NODE/Data Relationships With Mongo/Models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://127.0.0.1:27017/relationshipDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO Database CONNECTION SUCCESSFUL")
+    })
+    .catch(err => {
+        console.log("MONGO Database ERROR")
+        console.log(err)
+    })
+
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses: [
+        {
+            street: String,
+            city: String,
+            state: String,
+            country: String
+        }
+    ]
+})
+
+const User = mongoose.model('User', userSchema);
+
+const makeUser = async () => {
+    const u = new User({
+        first: 'Harry',
+        last: 'Potter',
+    })
+    u.addresses.push({
+        street: '123 Sesame Street',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    })
+    const res = await u.save()
+    console.log(res)
+}
+
+const addAddress = async (id) => {
+    const user = await User.findById(id);
+    user.addresses.push(
+        {
+            street: '99 Alpine Rd',
+            city: 'Hampshire',
+            state: 'CA',
+            country: 'USA'
+        }
+    )
+    const res = await user.save()
+    console.log(res);
+}
+
+// makeUser();
+addAddress('6415128eb9e2f9bd52291902');
