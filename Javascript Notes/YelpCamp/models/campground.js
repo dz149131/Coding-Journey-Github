9d31771de7dec68619faea66c7d5a899fb755c55
Danny Zhang <dz149131@gmail.com>
2023-03-25 17:12:22 -0500

    Delete Middleware notes

--- Javascript Notes/YelpCamp/models/campground.js
+++ Javascript Notes/YelpCamp/models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -14,6 +15,17 @@ const CampgroundSchema = new Schema({
         }
     ]
 });
+
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 const Campground = mongoose.model('Campground', CampgroundSchema);
 
