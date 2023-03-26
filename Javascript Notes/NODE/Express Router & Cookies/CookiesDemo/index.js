ddea61aede44ad43463704c72e28b0bb4a2b7c78
Danny Zhang <dz149131@gmail.com>
2023-03-26 18:59:03 -0500

    Signing Cookies notes

--- Javascript Notes/NODE/Express Router & Cookies/CookiesDemo/index.js
+++ Javascript Notes/NODE/Express Router & Cookies/CookiesDemo/index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+app.use(cookieParser('cookiepassword'));
 
 app.get('/greet', (req, res) => {
-    console.log(req.cookies)
     const {name = 'No-name'} = req.cookies;
     res.send(`Hey There, ${name}`)
 })
@@ -13,6 +12,17 @@ app.get('/greet', (req, res) => {
 app.get('/setname', (req, res) => {
     res.cookie('name', 'Alfred');
     res.send('cookie sent')
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', {signed: true});
+    res.send('cookie signed')
+})
+
+app.get('/verifyfruit', (req, res) => {
+    console.log(req.cookies)
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
 })
 
 app.listen(3000, () => {
