06da97cc37e0dd4368578d687d5f66d6b50f0b22
Danny Zhang <dz149131@gmail.com>
2023-03-29 16:35:26 -0500

    Session Practice notes

--- Javascript Notes/NODE/Express Session & Flash/index.js
+++ Javascript Notes/NODE/Express Session & Flash/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 
-app.use(session({secret: 'sessionsecret'}));
+const sessionOptions = {secret: 'sessionsecret', resave: false, saveUninitialized: false};
+app.use(session(sessionOptions));
 
 app.get('/viewcount', (req, res) => {
     if (req.session.count) {
@@ -12,6 +13,17 @@ app.get('/viewcount', (req, res) => {
     }
     res.send(`You have viewed this page ${req.session.count} times`)
 
+})
+
+app.get('/register', (req, res) => {
+    const {username = 'Anonymous'} = req.query;
+    req.session.username = username;
+    res.redirect('/greet') 
+})
+
+app.get('/greet', (req, res) => {
+    const {username} = req.session;
+    res.send(`Welcome back, ${username}`)
 })
 
 app.listen(3000, () => {
