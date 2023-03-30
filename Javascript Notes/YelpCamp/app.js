b90a9b2f13f974c6a448865b4364671f4b0e3ad6
Danny Zhang <dz149131@gmail.com>
2023-03-30 15:35:43 -0500

    Serving Static Assets notes

--- Javascript Notes/YelpCamp/app.js
+++ Javascript Notes/YelpCamp/app.js
@@ -28,7 +28,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
