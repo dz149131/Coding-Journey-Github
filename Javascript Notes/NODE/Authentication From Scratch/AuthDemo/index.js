7d7a4c4a2e2525f9dc6cfb3f6c817fa6b1c984d1
Danny Zhang <dz149131@gmail.com>
2023-03-31 17:13:34 -0500

    AuthDemo Register notes

--- Javascript Notes/NODE/Authentication From Scratch/AuthDemo/index.js
+++ Javascript Notes/NODE/Authentication From Scratch/AuthDemo/index.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const User = require('./models/user');
+const bcrypt = require('bcrypt');
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://127.0.0.1:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION SUCCESSFUL")
+    })
+    .catch(err => {
+        console.log("MONGO ERROR")
+        console.log(err)
+    })
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Home Page')
+})
+
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const {password, username} = req.body;
+    // await bcrypt.hash(req.body.password)
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User ({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/');
 })
 
 app.get('/secret', (req, res) => {
