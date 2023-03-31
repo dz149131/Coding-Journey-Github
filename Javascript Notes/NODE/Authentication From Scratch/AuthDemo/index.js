3ce57b874e261b3de262e89b9ca69e99dd499632
Danny Zhang <dz149131@gmail.com>
2023-03-31 18:08:22 -0500

    AuthDemo Staying Logged In With Sessions notes

--- Javascript Notes/NODE/Authentication From Scratch/AuthDemo/index.js
+++ Javascript Notes/NODE/Authentication From Scratch/AuthDemo/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const User = require('./models/user');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 mongoose.set("strictQuery", false);
 mongoose.connect('mongodb://127.0.0.1:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({extended: true}));
+app.use(session({secret: 'sessionsecret'}))
 
 app.get('/', (req, res) => {
     res.send('Home Page')
@@ -30,18 +32,27 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login')
 })
-
+//handles a login request by finding a user in the database, 
+//comparing the submitted password with the hashed password stored in the database, 
+//and either logging the user in and redirecting them to the /secret page 
+//or redirecting them back to the /login page if the login attempt was unsuccessful.
 app.post('/login', async (req, res) => {
     const {username, password} = req.body;
     const user = await User.findOne({username});
     const validPassword = await bcrypt.compare(password, user.password);
     if(validPassword){
-        res.send("Successfully Logged In!")
+        //if successfully logged in store user._id in session user_id
+        req.session.user_id = user._id;
+        res.redirect('/secret')
     } else {
-        res.send("Wrong Username Or Password")
+        res.redirect('/login')
     }
 })
 
+//registers a new user by hashing their password, 
+//creating a new user object, saving it to a database, 
+//setting a session variable to track the user, 
+//and redirecting the user to the homepage.
 app.post('/register', async (req, res) => {
     const {password, username} = req.body;
     // await bcrypt.hash(req.body.password)
@@ -51,11 +62,19 @@ app.post('/register', async (req, res) => {
         password: hash
     })
     await user.save();
+    req.session.user_id = user._id;
     res.redirect('/');
 })
 
+//checks whether a user is logged in and either redirects 
+//them to the login page or allows them to access the secret page 
+//with a message that can only be seen by logged-in users.
 app.get('/secret', (req, res) => {
-    res.send("You can only see me if logged in")
+    if(!req.session.user_id){
+        res.redirect('/login')
+    } else {
+        res.send("You can only see me if logged in")
+    }
 })
 
 app.listen(3000, () => {
