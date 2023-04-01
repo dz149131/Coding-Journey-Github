d2eb7a6c1323c660fb8781c7adcb9033e988bf88
Danny Zhang <dz149131@gmail.com>
2023-03-31 19:57:08 -0500

    Refactoring To Model Methods notes 1.1

--- Javascript Notes/NODE/Authentication From Scratch/AuthDemo/models/user.js
+++ Javascript Notes/NODE/Authentication From Scratch/AuthDemo/models/user.js
@@ -28,6 +28,17 @@ userSchema.statics.findAndValidate = async function (username, password) {
     return isValid ? foundUser : false;
 }
 
+// This code sets up a pre-save hook on a Mongoose schema called userSchema. A pre-save hook is a function that is executed before a document is saved to the database.
+
+// The pre method is used to define the hook and takes two arguments: 'save', which indicates that the hook should be triggered before a document is saved, and a callback function that will be executed when the hook is triggered.
+
+// The callback function takes a next parameter, which is a function that must be called when the hook is complete to continue with the saving process.
+
+// In this specific hook, the function checks whether the password field has been modified before saving the document. If it has, the bcrypt library's hash method is called with the password and a salt value of 12 to hash the password. The hashed password is then stored in the password field of the document.
+
+// If the password field has not been modified, the next function is called immediately, and the document is saved without any further modifications.
+
+// This hook ensures that passwords are always hashed before being stored in the database and is a good security measure to protect against password leaks in case the database is compromised.
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 12);
