a08b3cad7710770072847a22a4d28a1581b77287
Danny Zhang <dz149131@gmail.com>
2023-04-05 17:52:00 -0500

    Login Routes notes

--- Javascript Notes/YelpCamp/routes/users.js
+++ Javascript Notes/YelpCamp/routes/users.js
@@ -25,4 +25,17 @@ router.post(
 	})
 );
 
+router.get('/login', (req, res) => {
+	res.render('users/login');
+});
+
+router.post(
+	'/login',
+	passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+	(req, res) => {
+		req.flash('success', 'Welcome Back!');
+		res.redirect('/campgrounds');
+	}
+);
+
 module.exports = router;
