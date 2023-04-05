4d029a39d4a810f4369efa5468f5bfa29a21baea
Danny Zhang <dz149131@gmail.com>
2023-04-05 16:43:42 -0500

    Register Route Logic notes

--- Javascript Notes/YelpCamp/routes/reviews.js
+++ Javascript Notes/YelpCamp/routes/reviews.js
@@ -1,41 +1,48 @@
 const express = require('express');
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
 const catchAsync = require('../utility/catchAsync');
 const ExpressError = require('../utility/ExpressError');
-const {reviewSchema} = require('../schemas.js');
+const { reviewSchema } = require('../schemas.js');
 
 const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+	const { error } = reviewSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map((el) => el.message).join(',');
+		throw new ExpressError(msg, 400);
+	} else {
+		next();
+	}
+};
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Created a new review.')
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.post(
+	'/',
+	validateReview,
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		const review = new Review(req.body.review);
+		campground.reviews.push(review);
+		await review.save();
+		await campground.save();
+		req.flash('success', 'Created a new review.');
+		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
 
-router.delete('/:reviewId', catchAsync(async (req, res) => {
-    const {id, reviewId} = req.params;
-    //matches value for reviewId insdie 'reviews' array
-    //reviews is basically a array of ids
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully deleted review.')
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.delete(
+	'/:reviewId',
+	catchAsync(async (req, res) => {
+		const { id, reviewId } = req.params;
+		//matches value for reviewId insdie 'reviews' array
+		//reviews is basically a array of ids
+		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+		await Review.findByIdAndDelete(reviewId);
+		req.flash('success', 'Successfully deleted review.');
+		res.redirect(`/campgrounds/${id}`);
+	})
+);
 
 module.exports = router;
