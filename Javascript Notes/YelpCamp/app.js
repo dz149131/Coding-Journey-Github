18814002f01c44599ede26d15533c20d0d540b43
Danny Zhang <dz149131@gmail.com>
2023-04-05 16:18:49 -0500

    Register Form notes

--- Javascript Notes/YelpCamp/app.js
+++ Javascript Notes/YelpCamp/app.js
@@ -7,24 +7,28 @@ const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utility/ExpressError');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
-
-mongoose.set("strictQuery", false);
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("MONGO Database CONNECTION SUCCESSFUL")
-    })
-    .catch(err => {
-        console.log("MONGO Database ERROR")
-        console.log(err)
-    })
+mongoose.set('strictQuery', false);
+mongoose
+	.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('MONGO Database CONNECTION SUCCESSFUL');
+	})
+	.catch((err) => {
+		console.log('MONGO Database ERROR');
+		console.log(err);
+	});
 
 const app = express();
 
@@ -32,20 +36,20 @@ app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const sessionConfig = {
-    secret: 'sessionsecret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+	secret: 'sessionsecret',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
+};
 app.use(session(sessionConfig));
 app.use(flash());
 
@@ -59,37 +63,39 @@ passport.deserializeUser(User.deserializeUser());
 //deserializeUser() Generates a function that is used by Passport to deserialize users into the session
 
 app.use((req, res, next) => {
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})
-
-app.get('/fakeUser', async (req, res) => {
-    const user = new User({email: 'randomEmail@gmail.com', username: 'hiImbob123'});
-    const newUser = await User.register(user, 'password');
-    res.send(newUser);
-})
-
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
-
-
-app.get('/', (req, res) => {
-    res.render('home')
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
 });
 
+app.get('/fakeUser', async (req, res) => {
+	const user = new User({
+		email: 'randomEmail@gmail.com',
+		username: 'hiImbob123',
+	});
+	const newUser = await User.register(user, 'password');
+	res.send(newUser);
+});
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+
+app.get('/', (req, res) => {
+	res.render('home');
+});
 
 app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
+	next(new ExpressError('Page Not Found', 404));
+});
 
 //--------------ERROR HANDLER----------------
 app.use((err, req, res, next) => {
-    const {statusCode = 500, message = 'Something Went Wrong'} = err;
-    if(!err.message) err.message = 'Oh No, Something Went Wrong'
-    res.status(statusCode).render('error', {err});
-})
+	const { statusCode = 500, message = 'Something Went Wrong' } = err;
+	if (!err.message) err.message = 'Oh No, Something Went Wrong';
+	res.status(statusCode).render('error', { err });
+});
 
 app.listen(3000, () => {
-    console.log('Serving on Port 3000')
-})
+	console.log('Serving on Port 3000');
+});
