45f71cda2ce6aff5309893842fb2c228dd505fbf
Danny Zhang <dz149131@gmail.com>
2023-04-08 19:10:14 -0500

    Reviews/Users Controller Refactor

--- Javascript Notes/YelpCamp/routes/reviews.js
+++ Javascript Notes/YelpCamp/routes/reviews.js
@@ -1,40 +1,14 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
-const Campground = require('../models/campground');
 const Review = require('../models/review');
+const Campground = require('../models/campground');
+const reviews = require('../controllers/reviews');
 const catchAsync = require('../utility/catchAsync');
 const ExpressError = require('../utility/ExpressError');
 
-router.post(
-	'/',
-	isLoggedIn,
-	validateReview,
-	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id);
-		const review = new Review(req.body.review);
-		review.author = req.user._id;
-		campground.reviews.push(review);
-		await review.save();
-		await campground.save();
-		req.flash('success', 'Created a new review.');
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
-);
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
-router.delete(
-	'/:reviewId',
-	isLoggedIn,
-	isReviewAuthor,
-	catchAsync(async (req, res) => {
-		const { id, reviewId } = req.params;
-		//matches value for reviewId inside 'reviews' array
-		//reviews is basically a array of ids
-		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-		await Review.findByIdAndDelete(reviewId);
-		req.flash('success', 'Successfully deleted review.');
-		res.redirect(`/campgrounds/${id}`);
-	})
-);
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
 
 module.exports = router;
--- Javascript Notes/YelpCamp/routes/users.js
+++ Javascript Notes/YelpCamp/routes/users.js
@@ -3,60 +3,22 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utility/catchAsync');
 const User = require('../models/user');
+const users = require('../controllers/users');
 const { checkReturnTo } = require('../middleware');
 
-router.get('/register', (req, res) => {
-	res.render('users/register');
-});
+router.get('/register', users.renderRegister);
 
-router.post(
-	'/register',
-	catchAsync(async (req, res, next) => {
-		try {
-			const { email, username, password } = req.body;
-			const user = new User({ email, username });
-			const registeredUser = await User.register(user, password);
-			req.login(registeredUser, (err) => {
-				if (err) return next(err);
-				req.flash('success', 'Welcome To YelpCamp');
-				res.redirect('/campgrounds');
-			});
-			console.log(registeredUser);
-		} catch (e) {
-			req.flash('error', e.message);
-			res.redirect('register');
-		}
-	})
-);
+router.post('/register', catchAsync(users.register));
 
-router.get('/login', (req, res) => {
-	if (req.query.returnTo) {
-		req.session.returnTo = req.query.returnTo;
-	}
-	res.render('users/login');
-});
+router.get('/login', users.renderLogin);
 
 router.post(
 	'/login',
 	checkReturnTo,
 	passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
-	(req, res) => {
-		req.flash('success', 'Welcome Back!');
-		// sets the redirectUrl variable to the value of req.session.returnTo or /campgrounds. req.session.returnTo is a property set by a previous middleware function that saves the URL that the user was trying to access before being redirected to the login page.
-		const redirectUrl = res.locals.returnTo || '/campgrounds';
-		res.redirect(redirectUrl);
-	}
+	users.login
 );
 
-router.get('/logout', (req, res) => {
-	req.logout(function (err) {
-		if (err) {
-			req.flash('error', 'e.message');
-		} else {
-			req.flash('success', 'Successfully Logged Out!');
-			res.redirect('/campgrounds');
-		}
-	});
-});
+router.get('/logout', users.logout);
 
 module.exports = router;
