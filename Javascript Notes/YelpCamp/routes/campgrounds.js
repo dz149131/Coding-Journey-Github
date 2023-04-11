994ef304820fea36ee967d9fe6b85f09f01293fc
Danny Zhang <dz149131@gmail.com>
2023-04-10 21:08:00 -0500

    Storing Uploaded Image In Mongo notes

--- Javascript Notes/YelpCamp/routes/campgrounds.js
+++ Javascript Notes/YelpCamp/routes/campgrounds.js
@@ -12,11 +12,12 @@ const Campground = require('../models/campground');
 router
 	.route('/')
 	.get(catchAsync(campgrounds.index))
-	// .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-	.post(upload.array('image'), (req, res) => {
-		console.log(req.body, req.files);
-		res.send('it worked!');
-	});
+	.post(
+		isLoggedIn,
+		upload.array('image'),
+		validateCampground,
+		catchAsync(campgrounds.createCampground)
+	);
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
