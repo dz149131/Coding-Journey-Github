1f9e18ab080a43a1d38c0425b3cc7195f3142665
Danny Zhang <dz149131@gmail.com>
2023-04-11 19:44:06 -0500

    Adding Upload To Edit Page notes

--- Javascript Notes/YelpCamp/routes/campgrounds.js
+++ Javascript Notes/YelpCamp/routes/campgrounds.js
@@ -9,11 +9,18 @@ const upload = multer({ storage });
 
 const Campground = require('../models/campground');
 
-router.route('/').get(catchAsync(campgrounds.index)).post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
+router
+	.route('/')
+	.get(catchAsync(campgrounds.index))
+	.post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.route('/:id').get(catchAsync(campgrounds.showCampground)).put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground)).delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+router
+	.route('/:id')
+	.get(catchAsync(campgrounds.showCampground))
+	.put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
+	.delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
