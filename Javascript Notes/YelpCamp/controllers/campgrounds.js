46abcfe91f2fd3754cf68273f8cbebb78dedf7ed
Danny Zhang <dz149131@gmail.com>
2023-04-13 21:14:04 -0500

    GeoJSON notes

--- Javascript Notes/YelpCamp/controllers/campgrounds.js
+++ Javascript Notes/YelpCamp/controllers/campgrounds.js
@@ -20,15 +20,14 @@ module.exports.createCampground = async (req, res, next) => {
 			limit: 1,
 		})
 		.send();
-	res.send(geoData.body.features[0].geometry.coordinates);
-	// res.send('ok');
-	// const campground = new Campground(req.body.campground);
-	// campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename }));
-	// campground.author = req.user._id;
-	// await campground.save();
-	// console.log(campground);
-	// req.flash('success', 'Successfully made a new campground.');
-	// res.redirect(`/campgrounds/${campground._id}`);
+	const campground = new Campground(req.body.campground);
+	campground.geometry = geoData.body.features[0].geometry;
+	campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename }));
+	campground.author = req.user._id;
+	await campground.save();
+	console.log(campground);
+	req.flash('success', 'Successfully made a new campground.');
+	res.redirect(`/campgrounds/${campground._id}`);
 };
 
 module.exports.showCampground = async (req, res) => {
