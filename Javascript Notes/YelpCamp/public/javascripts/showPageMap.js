d87b742993708792f2174e445e71ddc4c4c3291b
Danny Zhang <dz149131@gmail.com>
2023-04-18 19:01:09 -0500

    Customize Map Popup notes

--- Javascript Notes/YelpCamp/public/javascripts/showPageMap.js
+++ Javascript Notes/YelpCamp/public/javascripts/showPageMap.js
@@ -6,4 +6,7 @@ const map = new mapboxgl.Map({
 	zoom: 9, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+new mapboxgl.Marker()
+	.setLngLat(campground.geometry.coordinates)
+	.setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`))
+	.addTo(map);
