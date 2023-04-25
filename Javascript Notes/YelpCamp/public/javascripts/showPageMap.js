3eaadd122f646032e9ca41b2ace1ae24b87d0d1f
Danny Zhang <dz149131@gmail.com>
2023-04-24 19:04:33 -0500

    Adding Map Controls

--- Javascript Notes/YelpCamp/public/javascripts/showPageMap.js
+++ Javascript Notes/YelpCamp/public/javascripts/showPageMap.js
@@ -6,6 +6,8 @@ const map = new mapboxgl.Map({
 	zoom: 9, // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
 new mapboxgl.Marker()
 	.setLngLat(campground.geometry.coordinates)
 	.setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`))
